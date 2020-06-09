4fa617aaff9e7ba368c77b4f500dd2e690f23779
Lanceli <47838732+laneston@users.noreply.github.com>
2020-06-09 20:03:32 +0800

    Revert "20200607-1"
    This reverts commit 5cd95a67bdb0d019907b8f915fc96530d6b60732.

--- Peripheral_BSP/stm32f4xx_dht22_bsp.c
+++ Peripheral_BSP/stm32f4xx_dht22_bsp.c
@@ -13,7 +13,7 @@
  
 #include "main.h"
 
-static u32 Timer_10us = 0;
+
 
 void TIM2_Init_us(u16 period)
 {
@@ -31,18 +31,18 @@ void TIM2_Init_us(u16 period)
 	
 	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
 	
+	TIM_Cmd(TIM2,DISABLE);
+	
 	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = TIM2_IRQnPriority;
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
-	
-	TIM_Cmd(TIM2,ENABLE);
 }
 
 void TIM2_IRQ(void)
 {
-	if(Timer_10us) Timer_10us--;
+	 
 }
 
 void DHT22_Init(void)
@@ -59,96 +59,10 @@ void DHT22_Init(void)
 	GPIO_Init(GPIO_DHT22, &GPIO_InitStructure);
 	
 	GPIO_DHT22_OUT = SET;/*set the pin level high*/
-	
-	TIM2_Init_us(timer2_period_10us);
 }
 
-static u8 DHT22_Start(void)
+u8 DTH22_ReadOnebit(void)
 {
-	GPIO_DHT22_OUT = RESET;
-	Timer_10us = 55;
-	while(Timer_10us);
-	GPIO_DHT22_OUT = SET;
-	Timer_10us = 4;
-	while(Timer_10us);
 	
-	Timer_10us = 10;
-	while((!GPIO_DHT22_IN) && Timer_10us);	
-	if(!Timer_10us) return RESET;
-	
-	Timer_10us = 10;
-	while(GPIO_DHT22_IN && Timer_10us);	
-	if(!Timer_10us) return RESET;
-	
-	return SET;
-}
-
-static u8 DTH22_GetOnebit(void)
-{
-	u8 revale;
-	
-	Timer_10us = 10;
-	while(!GPIO_DHT22_IN && Timer_10us);
-	if(!Timer_10us) return RESET;
-	
-	Timer_10us = 4;
-	while(GPIO_DHT22_IN && Timer_10us);
-	if(Timer_10us) revale = 0;
-	else revale = 1;
-	
-	return revale;
-}
-
-static u8 DHT22_GetOneByte(void)
-{
-	u8 data = 0;
-	u8 i;
-	
-	for(i=0; i<8; i++)
-	{
-		data <<= 1;
-		data |= DTH22_GetOnebit();
-	}
-	return data;
-}
-
-
-u8 DHT22_GetOneFrame(u8 *result)
-{
-	u8 revale;
-	u8 i;
-	u8 data[5];
-	
-	if(DHT22_Start())
-	{
-		
-	}
-	else
-	{
-		return 0xff;
-	}
-	
-	for(i=0; i<5; i++)
-	{
-		data[i] = DHT22_GetOneByte();
-	}
-	
-	revale = data[0] + data[1] + data[2] + data[3];
-	
-	if(revale == data[4])
-	{
-		for(i=0; i<4; i++)
-		{
-			*result = data[i];
-			result++;
-		}
-		revale = SET;
-	}
-	else
-	{
-		revale = RESET;
-	}
-	
-	return revale;
 }
 
